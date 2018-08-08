20dd3cece5d145c4b873a7c73fba4b59e01d1e03
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-08-08 15:10:20 +0530

    Added lodash example to remove duplicates from an array

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ console.log('Starting app...');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
-var result = notes.add(45,12);
+var unique_arr = _.uniq(['sourabh', 'city', 'city', 1, 4, 'sourabh', 4, 5])
 
-console.log(result);
+console.log(unique_arr);
+
+// var result = notes.add(45,12);
+
+// console.log(result);
 
 // var user = os.userInfo();
 
