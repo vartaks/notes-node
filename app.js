cc9f258aa361ac1e0647ee94929fbdfb1a4472e2
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-08-10 14:47:50 +0530

    Added get all notes functionality

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app...');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -10,7 +8,7 @@ const argv = yargs.argv;
 
 var command = argv._[0];
 
-console.log('Command: ', command);
+// console.log('Command: ', command);
 // console.log('Yargs', argv);
 
 if (command === 'add') {
@@ -23,7 +21,10 @@ if (command === 'add') {
         console.log('Cannot add duplicate note with title', argv.title);
     }
 } else if (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
     var note = notes.getNote(argv.title);
 
