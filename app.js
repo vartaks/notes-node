046fc4bcaa2f930da325488916b067e48b4d7eef
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-08-10 09:57:12 +0530

    Added more notes functionalities

--- app.js
+++ app.js
@@ -11,16 +11,34 @@ const argv = yargs.argv;
 var command = argv._[0];
 
 console.log('Command: ', command);
-console.log('Yargs', argv);
+// console.log('Yargs', argv);
 
 if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+
+    if (note) {
+        console.log("Note created");
+        notes.logNote(note);
+    } else {
+        console.log('Cannot add duplicate note with title', argv.title);
+    }
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
-    notes.getNote(argv.title);
+    var note = notes.getNote(argv.title);
+
+    if (note) {
+        console.log("Note found");
+        notes.logNote(note);
+    } else {
+        console.log('Note not found with title', argv.title);
+    }
 } else if (command === 'remove') {
-    notes.removeNote(argv.title);
+    var noteRemoved = notes.removeNote(argv.title);
+
+    var message = noteRemoved ? 'Note was removed' : 'Note not found';
+
+    console.log(message);
 } else {
     console.log('Command not found');
 }
--- notes.js
+++ notes.js
@@ -2,30 +2,33 @@ console.log('Starting notes module...');
 
 const fs = require('fs');
 
-var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+var fetchNotes = () => {
+    try {   
+        var strNotes = fs.readFileSync('notes-data.json');
+        return JSON.parse(strNotes);
+    } catch(e) {
+        return [];
+    }
+};
 
-    var notes = [];
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
+var addNote = (title, body) => {
+    var notes = fetchNotes();
 
     var note = {
         title,
         body
     };
 
-    try {   
-        var strNotes = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(strNotes);
-    } catch(e) {
-
-    }
-
     var duplicateNotes = notes.filter((note) => note.title === title);
 
     if (duplicateNotes.length === 0) {
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-    } else {
-        console.log('Cannot add duplicate note.');
+        saveNotes(notes);
+        return note;
     }
 };
 
@@ -34,16 +37,31 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-    console.log('Getting note ', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
 };
 
 var removeNote = (title) => {
     console.log('Removing note', title);
+
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
+
+    return notes.length !== filteredNotes.length;
+};
+
+var logNote = (note) => {
+    console.log("----")
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
 };
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
